4224b0504dda9913cd8712f0b9867b82bc99e16a
Josiah Swanson <josswanson@westmont.edu>
2021-03-08 21:39:07 -0800

    Small bug fix

--- server.js
+++ server.js
@@ -5,25 +5,26 @@ var static = require('node-static');
 var http = require('http');
 
 /* Assume that we are running on Heroku */
-var port = process.env.POT;
+var port = process.env.PORT;
 var directory = __dirname + '/public';
 
 /* If we aren't on Heroku, then we need to reudjust the port and directory */
-if(typeof port == 'undefined' || !port) {
+if (typeof port == 'undefined' || !port) {
     directory = './public';
     port = 8080;
 }
 
-/* SEt up a static web-server that will deliver files from the filesystem */
+/* Set up a static web-server that will deliver files from the filesystem */
 var file = new static.Server(directory);
 
 /* Construct an http server that gets files from the file server */
 var app = http.createServer(
-    function(request, response) {
-        request.addListener('end', 
-        function() {
-             file.serverInfo(request, response);
-        }).resume();
+    function (request, response) {
+        request.addListener('end',
+            function () {
+                file.serve(request, response);
+            }
+        ).resume();
     }
 ).listen(port);
 
